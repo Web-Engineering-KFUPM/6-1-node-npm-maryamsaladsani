a60c4f987bacdb92c5073e3fe79ad091aec82045
maryamsaladsani <s202263480@kfupm.edu.sa>
2025-11-05 12:00:06 +0300

    TODO1 is Done TODO2 is Done TODO3 is Done TODO4 is Done

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,21 @@
+// TODO 4
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  return numbers.slice(1).reduce((result, num) => result - num, numbers[0]);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((product, num) => product * num, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+  if (numbers.slice(1).includes(0)) {
+    console.log("Error: Division by zero.");
+    return NaN;
+  }
+  return numbers.slice(1).reduce((result, num) => result / num, numbers[0]);
 }
 
